fdfd6cae51fc579d764241ada2451e547539ffbf
phillip mahar <mcephil@gmail.com>
2019-03-05 18:46:02 -0600

    Convert to MVC design pattern

---
+++ Todoey/Data Model/item.swift
@@ -0,0 +1,17 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Phillip Mahar on 3/4/19.
+//  Copyright © 2019 MaharTech. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
